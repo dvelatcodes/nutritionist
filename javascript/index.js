e15032acea68e54df1449a750a00ac42fbed52f7
love olubummo <olubummolove3@gmail.com>
2025-07-04 02:15:44 +0100

    feature: worked on hompage

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,5 @@
 let hamburger = document.querySelector('.hamburger');
+let navLinks = document.querySelector('.nav-links');
 let hamChecker = false;
 
 
@@ -6,10 +7,12 @@ let hamChecker = false;
 hamburger.addEventListener('click', (e)=>{
     if (hamChecker == false) {
         hamburger.style.transform = "rotate(-180deg)";
+        navLinks.style.display = 'flex';
         hamChecker = true
     }
     else{
         hamburger.style.transform = "rotate(360deg)";
+        navLinks.style.display = 'none';
         hamChecker = false
     }
 })
