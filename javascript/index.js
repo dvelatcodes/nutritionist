09a3dbf8b60d82300863595d90dd47e32be35bd3
love olubummo <olubummolove3@gmail.com>
2025-07-04 16:18:07 +0100

    feature: worked on hompage

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,28 @@
 let hamburger = document.querySelector('.hamburger');
-let navLinks = document.querySelector('.nav-links');
+let navLinks = document.querySelector('.navigation-container');
 let hamChecker = false;
 let pages = document.querySelectorAll('.pages');
+let header = document.querySelector('header');
 
 
 
+// function to toggle the nav screens for mobile view
 hamburger.addEventListener('click', (e)=>{
     if (hamChecker == false) {
         hamburger.style.transform = "rotate(-180deg)";
-        navLinks.style.transform = 'translateX(0)';
+        navLinks.style.right = '0';
         hamChecker = true;
     }
     else{
         hamburger.style.transform = "rotate(360deg)";
-        navLinks.style.transform = 'translateX(100vw)';
+        navLinks.style.right = '-100vw';
         hamChecker = false;
     }
 })
 
 
+
+// function to showcase active navigation link
 pages.forEach((page)=>{
     let activePage = page.getAttribute('href');
     let url = window.location.pathname;
