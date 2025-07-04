0fa33a09d82f4b65a947105bb34fe5ed21be92eb
love olubummo <olubummolove3@gmail.com>
2025-07-04 11:48:11 +0100

    feature: worked on hompage

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,28 @@
 let hamburger = document.querySelector('.hamburger');
 let navLinks = document.querySelector('.nav-links');
 let hamChecker = false;
+let pages = document.querySelectorAll('.pages');
 
 
 
 hamburger.addEventListener('click', (e)=>{
     if (hamChecker == false) {
         hamburger.style.transform = "rotate(-180deg)";
-        // navLinks.style.display = 'flex';
-        navLinks.style.transform = 'translateX(0)'
-        hamChecker = true
+        navLinks.style.transform = 'translateX(0)';
+        hamChecker = true;
     }
     else{
         hamburger.style.transform = "rotate(360deg)";
-        // navLinks.style.display = 'none';
-        navLinks.style.transform = 'translateX(100vw)'
-        hamChecker = false
+        navLinks.style.transform = 'translateX(100vw)';
+        hamChecker = false;
+    }
+})
+
+
+pages.forEach((page)=>{
+    let activePage = page.getAttribute('href');
+    let url = window.location.pathname;
+    if (activePage == window.location.pathname) {
+        page.style.color = '#CBEA7B';
     }
 })
